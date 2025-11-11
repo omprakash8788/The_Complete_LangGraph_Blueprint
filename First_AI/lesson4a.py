78437b39243a38683bddd4f53dc89b5923eb06a1
omprakash8788 <omprakashkumar552@gmail.com>
2025-11-11 22:01:45 +0530

    setupp

---
+++ First_AI/lesson4a.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+
+import os
+# Load environmet variable from .env file
+load_dotenv()
+# Get the API key from environment variables
+api_key = os.getenv("OPENAI_API_KEY")
+# Check if API key exists 
+if api_key is None:
+    print("Error: OPENAI_API_KEY not found in environment variables")
+else:
+    print("API key loaded successfully")
+
